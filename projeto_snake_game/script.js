d8b2b2a0dbe411ddade6adc4115e610ef9834454
WillianVieira89 <wilianleoanrdo@hotmail.com>
2021-07-07 23:20:07 -0300

    cria comida para a cobrinha

--- projeto_snake_game/script.js
+++ projeto_snake_game/script.js
@@ -7,10 +7,14 @@ snake[0] = {
     y: 8 * box
 }
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBG() {
     context.fillStyle = "lightgreen";
-    context.fillRect(0, 0, 16 * box * box);
+    context.fillRect(0, 0, 16 * box , 16 * box);
 }
 
 function criarCobrinha() {
@@ -18,6 +22,11 @@ function criarCobrinha() {
         context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box)
     }
+}
+
+function drawFood() {
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box)
 }
 
 document.addEventListener("keydown", update);
@@ -38,6 +47,7 @@ function iniciarJogo() {
 
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
